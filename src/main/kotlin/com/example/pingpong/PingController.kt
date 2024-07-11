824069d5cb65ca872a6ce2c5a0429686e5bb66b3
Shanmukh-C <shanmudhanu123@gmail.com>
2024-07-11 22:25:36 +0530

    Code Upload

---
+++ src/main/kotlin/com/example/pingpong/PingController.kt
@@ -0,0 +1,15 @@
+package com.example.pingpong
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PingController {
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong";
+    }
+}
